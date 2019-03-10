a3344da37d48ba60baeb464ef68b1560bc9a5697
Alymbekov <maxim.makarov.1997@mail.ru>
2019-03-11 01:44:14 +0600

    add env example

--- crm_food/models.py
+++ crm_food/models.py
@@ -1,3 +1,45 @@
-from django.db import models
-
-# Create your models here.
+# from django.db import models
+#
+#
+# class Table(models.Model):
+#     name_of_tables = models.PositiveIntegerField()
+#
+#
+#     def __str__(self):
+#         return self.name_of_tables
+#
+#
+# class Order(models.Model):
+#     tables = models.ForeignKey()
+#     status = models.BooleanField()
+#     isitopen = models.PositiveIntegerField()
+#     date_of_order = models.DateTimeField()
+#     meals = models.ForeignKey()
+#
+#
+# class Department(model.Models):
+#     name_of_departments = models.CharField(max_length=155)
+#
+#
+#     def __str__(self):
+#         return self.name_of_departments
+#
+#
+# class Role(models.Model):
+#     name_of_tables = models.CharField(max_length=155)
+#
+#
+# class Meal(model.Model):
+#     name_of_meals = models.CharField(max_length=100)
+#     price = models.FloatField()
+#     description = models.TextField()
+#
+#
+# class MealCategories(models.Model):
+#     name_of_categories = models.CharField(max_length=100)
+#     departments = models.()
+#
+#
+#
+#
+# # Create your models here.

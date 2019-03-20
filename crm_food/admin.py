4688092199890941276cb7908571699fc8aec8ea
Alymbekov <maxim.makarov.1997@mail.ru>
2019-03-20 20:15:00 +0600

    registered models status,order,tables,departments,roles

--- crm_food/admin.py
+++ crm_food/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from crm_food.models import (
+                         Table,Order,
+                         Department,
+                         Role,Status
+                      )
+
+
+admin.site.register(Table)
+admin.site.register(Order)
+admin.site.register(Department)
+admin.site.register(Role)
+admin.site.register(Status)
 
 # Register your models here.
